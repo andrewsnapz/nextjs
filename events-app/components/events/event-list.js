7c5729719fef053e06eb81f1f705612c0f356b83
Andrew Shin <andyrpg@gmail.com>
2023-06-05 19:28:58 -0700

    added events-app

---
+++ events-app/components/events/event-list.js
@@ -0,0 +1,19 @@
+import EventItem from "./event-item";
+import classes from "./event-list.module.css";
+
+export default function EventList({ items }) {
+  return (
+    <ul className={classes.list}>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          title={event.title}
+          image={event.image}
+          date={event.date}
+          location={event.location}
+          id={event.id}
+        />
+      ))}
+    </ul>
+  );
+}
