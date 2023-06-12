ec543c26dfffbaef0d1ae5c0bdacabbbe6b5996d
Andrew Shin <andyrpg@gmail.com>
2023-06-11 23:47:29 -0700

    finished events-app-api

---
+++ events-app-api/pages/api/newsletter.js
@@ -0,0 +1,34 @@
+import { MongoClient } from "mongodb";
+import { connectDataBase, insertDocument } from "../../helpers/db-util";
+
+// 1 POST REQUEST
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email } = req.body;
+
+    if (!email || !email.includes("@")) {
+      res.status(422).json({ message: "invalid email address" });
+      return;
+    }
+
+    let client;
+
+    try {
+      client = await connectDataBase();
+    } catch (error) {
+      res.status(500).json({ message: "connecting to the database failed!" });
+      return;
+    }
+
+    try {
+      await insertDocument(client, "newsletter", { email });
+    } catch (error) {
+      res.status(500).json({ message: "inserting data failed!" });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({ message: "signed up!" });
+  }
+}
