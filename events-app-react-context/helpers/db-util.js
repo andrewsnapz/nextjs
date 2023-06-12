28fc44efe3bc37960971365d531855edbba695a2
Andrew Shin <andyrpg@gmail.com>
2023-06-12 06:46:19 -0700

    added react-context

---
+++ events-app-react-context/helpers/db-util.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+import { dbConnectionString } from "../config";
+
+export async function connectDataBase() {
+  const client = await MongoClient.connect(dbConnectionString);
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+  return documents;
+}
